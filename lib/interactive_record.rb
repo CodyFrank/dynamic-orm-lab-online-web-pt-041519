12927d557f57df072ae807b7a0c7b74d98cfea46
Cody Frank <cody.frank30@gmail.com>
2019-07-04 16:05:11 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -61,5 +61,9 @@ class InteractiveRecord
     DB[:conn].execute(sql, name)
   end
 
+  def self.find_by(hash)
+    sql = "SELECT * FROM #{self.table_name} WHERE ? = ?"
+    DB[:conn].execute(sql, )
+  end
 
 end
